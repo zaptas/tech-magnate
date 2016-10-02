1fd1bcff1f9a6afdb7eacd5aec102b2f648b0f7e
sohel khan <sohelkhan00000@gmail.com>
2016-10-02 19:10:46 +0530

    dsd

---
+++ wayzata/assets/js/script.js
@@ -0,0 +1,47 @@
+﻿
+
+
+
+$(document).ready(function () {
+    "use strict";
+    Onload();
+    function Onload() {
+        var temp = $(window).width();
+        if (temp < 767) {
+            $("#main-manu a").removeClass(" menu-active");
+            // $("#main-manu , #main-manu .small-menu").css({ "background-color": "lightgrey" });
+        }
+    }
+    $("a").on("click", function () {
+        if ($(window).width() > 767) {
+            $("#main-manu a").removeClass(" menu-active");
+            $(this).addClass("menu-active");
+        }
+    });
+
+
+    $("#main-manu .small-menu").click(function () {
+        $("#main-manu .menu-wrapper").slideToggle(500);
+    });
+    $("#main-manu .menu-wrapper a").click(function () {
+        var temp = $(window).width();
+        if (temp > 767) {
+        }
+        else if (temp <= 767) {
+
+            $("#main-manu a").removeClass("menu-active");
+            $("#main-manu .menu-wrapper").css({ "display": "none" });
+        }
+
+    });
+    $(window).resize(function () {
+        var temp = $(window).width();
+        if (temp > 767) {
+            //$("#main-manu .menu-wrapper").css("display", "block");
+        }
+        else if (temp <= 767) {
+            $("#main-manu a").removeClass(" menu-active");
+
+        }
+    });
+});
